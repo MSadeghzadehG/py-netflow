a1ca8761d17ad63b9c7f247f0ba092c7df5a6ac6
MSadeghzadehG <mahdisadeghzadeh24@gmail.com>
2020-07-25 05:27:57 +0430

    Complete discovery service

--- discovery.py
+++ discovery.py
@@ -2,8 +2,11 @@ import socket
 import sys
 import ast
 
+from time import sleep
+from threadpool import ThreadPool
 
-class Discovery():
+
+class DiscoveryService():
 
     def __init__(self, ip, port, initial_nodes, period):
         self.period = period
@@ -15,6 +18,15 @@ class Discovery():
         self.client_discovery_socket = socket.socket(
             family=socket.AF_INET, type=socket.SOCK_DGRAM
         )
+        self.threadpool = ThreadPool(2)
+        self.threadpool.add_task(self.discovery_job)
+        self.threadpool.add_task(self.start_discovery_server)
+        self.threadpool.wait_completion()
+
+    def discovery_job(self):
+        while True:
+            self.send_nodes_to_others()
+            sleep(self.period)
 
     def update_nodes(self, discovered_list):
         for discovered_node in discovered_list:
@@ -23,6 +35,9 @@ class Discovery():
                 discovered_node is not self.ip
             ):
                 self.nodes.append(discovered_node)
+
+    def get_nodes(self):
+        return self.nodes
 
     def start_discovery_server(self):
         # Create a datagram socket
@@ -33,7 +48,10 @@ class Discovery():
         print(server_discovery_socket)
         server_identifier = (self.ip, self.port)
         server_discovery_socket.bind(server_identifier)
-        print("discovery server up and listening on " + str(server_identifier))
+        print(
+            "Discovery service's server up and listening on " +
+            str(server_identifier)
+        )
         # Listen for incoming datagrams
         while(True):
             message_pair = server_discovery_socket.recvfrom(
@@ -42,7 +60,10 @@ class Discovery():
             message = message_pair[0]
             address = message_pair[1]
             self.update_nodes(ast.literal_eval(message.decode('utf-8')))
-            print(self.nodes)
+            print(
+                "Nodes list updated by Discovery service's server: " +
+                str(self.nodes)
+            )
 
     def send_nodes_to_others(self):
         message = str(self.nodes)
--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-from discovery import Discovery
+from discovery import DiscoveryService
 from threadpool import ThreadPool
 
 
 def main():
     main_pool = ThreadPool(2)
-
-    discovery_service = Discovery("127.0.0.1", 3000, ["0.0.0.0", "localhost"], 5)
-    main_pool.add_task(discovery_service.start_discovery_server)
-    for i in range(10):
-        discovery_service.send_nodes_to_others()
+    discovery_service = DiscoveryService(
+        ip="127.0.0.1",
+        port=3000,
+        initial_nodes=["0.0.0.0", "localhost"],
+        period=5
+    )
     main_pool.wait_completion()
 
 if __name__ == "__main__":
