63a78e52bfb1b581624efebccdc68a0b39fdf042
MSadeghzadehG <mahdisadeghzadeh24@gmail.com>
2020-07-25 08:41:55 +0430

    Add CheckFileService

---
+++ checkFile.py
@@ -0,0 +1,60 @@
+from socket import timeout
+from os import path
+from time import time
+
+from threadPool import ThreadPool
+from udpService import UDPService
+
+
+class CheckFileService(UDPService):
+
+    def __init__(self, ip, port, nodes, timeout, directory):
+        super(CheckFileService, self).__init__(
+            name='CheckFile',
+            ip=ip,
+            port=port
+        )
+        self.nodes = nodes
+        self.directory = directory
+        self.client_socket.settimeout(timeout)
+        self.threadpool = ThreadPool(2)
+
+    def start_service(self):
+        self.threadpool.add_task(self.start_server)
+        self.threadpool.wait_completion()
+
+    def process_server_response(self, message, address):
+        file_existance = str(self.check_file_existance(message))
+        self.send_message(
+            str_message=file_existance,
+            address=address,
+            socket=self.server_socket
+        )
+
+    def check_file_existance(self, file_path):
+        return path.exists(self.directory + '/' + file_path)
+
+    def get_nodes_response_times(self, file_path):
+        message = file_path
+        response_times = []
+        for node in self.nodes:
+            if node is not self.ip:
+                address = (node, self.port)
+                start_time = time()
+                self.send_message(
+                    str_message=message,
+                    address=address,
+                    socket=self.client_socket
+                )
+                try:
+                    response = self.get_message(self.client_socket)
+                    print(response)
+                    if response["message"] == "True":
+                        response_times.append((node, time() - start_time))
+                except timeout:
+                    pass
+        print(response_times)
+        if response_times:
+            return min(response_times, key=lambda t: t[1])[0]
+        else:
+            return None
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+from checkFile import CheckFileService
 from discovery import DiscoveryService
 from threadPool import ThreadPool
 
 
 def main():
-    node_list = ["127.0.0.1", "localhost"]
+    node_list = ["0.0.0.0", "localhost"]
     main_pool = ThreadPool(2)
     discovery_service = DiscoveryService(
         ip="127.0.0.1",
@@ -11,8 +12,16 @@ def main():
         initial_nodes=node_list,
         period=5
     )
+    check_file_service = CheckFileService(
+        ip="127.0.0.1",
+        port=3001,
+        nodes=node_list,
+        timeout=5,
+        directory='files/'
+    )
     main_pool.add_task(discovery_service.start_service)
-    
+    main_pool.add_task(check_file_service.start_service)
+    print(check_file_service.get_nodes_response_times('hello.txt'))
     main_pool.wait_completion()
 
 if __name__ == "__main__":
